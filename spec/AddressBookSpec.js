4c63992d1165f26e66fcae3ec794002a4c2cb40a
samanthablasbalg <blasbalgs@gmail.com>
2016-11-03 16:20:21 -0400

    Add asynchronous testpoint

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,7 +1,13 @@
 describe('Address Book', function() {
+	var addressBook,
+		thisContact;
+
+	beforeEach(function() {
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	});
+
 	it('should be able to add a contact', function() {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
 
 		addressBook.addContact(thisContact);
 
@@ -9,12 +15,26 @@ describe('Address Book', function() {
 	});
 
 	it('should be able to delete a contact', function () {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
 
 		addressBook.addContact(thisContact);
 		addressBook.deleteContact(0);
 
 		expect(addressBook.getContact(0)).not.toBeDefined();
-	})
+	});
+});
+
+describe('Async Address Book', function() {
+	var addressBook = new AddressBook();
+
+	beforeEach(function(done) {
+		addressBook.getInitialContacts(function () {
+			done();
+		});
+	});
+
+	it('should grab initial contacts', function (done) {
+
+		expect(addressBook.initialComplete).toBe(true);
+		done();
+	});
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,6 +1,18 @@
 var AddressBook = function () {
 	this.contacts = [];
+	this.initialComplete = false;
 };
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+	var self = this;
+
+	setTimeout(function() {
+		self.initialComplete = true;
+		if (cb) {
+			return cb();
+		}
+	}, 3);
+}
 
 AddressBook.prototype.addContact = function(contact) {
 	this.contacts.push(contact);
